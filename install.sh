688615a009917a9a9eb150f79b5a38cfc85d2000
Sami J. Mäkinen <sjm@iki.fi>
2021-10-10 21:39:20 +0300

    installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+set -e
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
+
+tgt=$HOME/urllog
+
+mkdir -p $tgt
+cd target/release
+rsync -var irssi-urlharvest urllog-meta urllog-generator urllog-search $tgt/
+
+exit 0
+# EOF
